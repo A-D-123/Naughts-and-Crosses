350f502955345ba6be89d3225af8776312da84d3
A-D-123 <alexdovey2001@gmail.com>
2025-09-17 12:24:49 +0100

    Add files via upload

---
+++ Naughts and Crosses (good AI)/Naughts and Crosses (good AI)/AI.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Naughts_and_Crosses__good_AI_
+{
+    class AI
+    {
+        public AI()
+        {
+            
+        }
+        public void Input(char[,] Boardpos, out int Besti, out int Bestj)
+        {
+            int[,] Score = new int[3, 3];
+            Besti = 0;
+            Bestj = 0;
+
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    if (Boardpos[i, j] != ' ')
+                    {
+                        Score[i, j] = -1000;
+                    }
+                    else
+                    {
+                        if ((i % 2 == 1 && j % 2 == 0) || (i % 2 == 0 && j % 2 == 1))
+                        {
+                            Score[i, j] += 1;
+                        }
+                        else if (i % 2 == 0 && j % 2 == 0)
+                        {
+                            Score[i, j] += 2;
+                        }
+                        else if (i == 1 && j == 1)
+                        {
+                            Score[i, j] += 3;
+                        }
+
+                        if (i == 0)
+                        {
+                            if (Boardpos[1, j] == 'X' && Boardpos[2, j] == 'X')
+                            {
+                                Score[i, j] += 50;
+                            }
+                            else if (Boardpos[1, j] == 'O' && Boardpos[2, j] == 'O')
+                            {
+                                Score[i, j] += 100;
+                            }
+                        }
+                        else if (i == 1)
+                        {
+                            if (Boardpos[0, j] == 'X' && Boardpos[2, j] == 'X')
+                            {
+                                Score[i, j] += 50;
+                            }
+                            else if (Boardpos[0, j] == 'O' && Boardpos[2, j] == 'O')
+                            {
+                                Score[i, j] += 100;
+                            }
+                        }
+                        else if (i == 2)
+                        {
+                            if (Boardpos[0, j] == 'X' && Boardpos[1, j] == 'X')
+                            {
+                                Score[i, j] += 50;
+                            }
+                            else if (Boardpos[0, j] == 'O' && Boardpos[1, j] == 'O')
+                            {
+                                Score[i, j] += 100;
+                            }
+                        }
+
+                        if (j == 0)
+                        {
+                            if (Boardpos[i, 1] == 'X' && Boardpos[i, 2] == 'X')
+                            {
+                                Score[i, j] += 50;
+                            }
+                            else if (Boardpos[i, 1] == 'O' && Boardpos[i, 2] == 'O')
+                            {
+                                Score[i, j] += 100;
+                            }
+                        }
+                        else if (j == 1)
+                        {
+                            if (Boardpos[i, 0] == 'X' && Boardpos[i, 2] == 'X')
+                            {
+                                Score[i, j] += 50;
+                            }
+                            else if (Boardpos[i, 0] == 'O' && Boardpos[i, 2] == 'O')
+                            {
+                                Score[i, j] += 100;
+                            }
+                        }
+                        else if (j == 2)
+                        {
+                            if (Boardpos[i, 0] == 'X' && Boardpos[i, 1] == 'X')
+                            {
+                                Score[i, j] += 50;
+                            }
+                            else if (Boardpos[i, 0] == 'O' && Boardpos[i, 1] == 'O')
+                            {
+                                Score[i, j] += 100;
+                            }
+                        }
+
+                        if (i == 0 && j == 0)
+                        {
+                            if (Boardpos[1, 1] == 'X' && Boardpos[2, 2] == 'X')
+                            {
+                                Score[i, j] += 50;
+                            }
+                            else if (Boardpos[1, 1] == 'O' && Boardpos[2, 2] == 'O')
+                            {
+                                Score[i, j] += 100;
+                            }
+                        }
+                        else if (i == 2 && j == 0)
+                        {
+                            if (Boardpos[1, 1] == 'X' && Boardpos[0, 2] == 'X')
+                            {
+                                Score[i, j] += 50;
+                            }
+                            else if (Boardpos[1, 1] == 'O' && Boardpos[0, 2] == 'O')
+                            {
+                                Score[i, j] += 100;
+                            }
+                        }
+                    }
+                }
+            }
+
+            int BestScore = 0;
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    if (Score[i, j] > BestScore)
+                    {
+                        BestScore = Score[i, j];
+                        Besti = i;
+                        Bestj = j;
+                    }
+                }
+            }
+        }
+    }
+}
+
---
+++ Naughts and Crosses (good AI)/Naughts and Crosses (good AI)/Program.cs
@@ -0,0 +1,192 @@
+﻿using System;
+
+namespace Naughts_and_Crosses__good_AI_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AI ai = new AI();
+            char Turn = RandomTurn();
+            char[,] Boardpos = new char[3, 3];
+            bool GameComplete = false;
+            bool AIPlaying = false;
+            int Besti, Bestj;
+
+            AIPlaying = PlayAgainstAI();
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Boardpos[i, j] = ' ';
+                }
+            }
+
+            while (GameComplete == false)
+            {
+                if (GameComplete == false)
+                {
+                    if (AIPlaying == false || Turn == 'X')
+                    {
+                        PlayerInput(Turn, Boardpos);
+                    }
+                    else
+                    {
+                        ai.Input(Boardpos, out Besti, out Bestj);
+                        Boardpos[Besti, Bestj] = 'O';
+                    }
+
+                    GameComplete = GameEndCheck(Turn, Boardpos, GameComplete);
+                    Turn = NextTurn(Turn);
+                }
+            }
+        }
+        static char RandomTurn()
+        {
+            Random rnd = new Random();
+            return (rnd.Next(0, 2) == 0) ? 'X' : 'O';
+        }
+        static bool PlayAgainstAI()
+        {
+            while (true)
+            {
+                Console.WriteLine("Would you like to play against an AI? y/n");
+                String Temp = Console.ReadLine();
+            
+                if (Temp.ToLower() == "y")
+                {
+                    return true;
+                }
+                else if (Temp.ToLower() == "n")
+                {
+                    return false;
+                }
+                else if (Temp.ToLower() == "y/n")
+                {
+                    Console.WriteLine("ts nga pmo");
+                    Console.ReadKey();
+                    Console.Clear();
+            }
+                else
+                {
+                    Console.WriteLine("Please input y/n");
+                    Console.ReadKey();
+                    Console.Clear();
+                }
+            }
+        }
+        static void SpawnBoard(char[,] Boardpos)
+        {
+            Console.Clear();
+            Console.WriteLine("   1   2   3");
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine($"{i + 1}  {Boardpos[0, i]} | {Boardpos[1, i]} | {Boardpos[2, i]}");
+                Console.WriteLine((i == 2) ? "" : "  ---+---+---");
+            }
+        }
+        static void PlayerInput(char Turn, char[,] Boardpos)
+        {
+                bool TurnComplete = false;
+
+            while (TurnComplete == false)
+            {
+                try
+                {
+                    SpawnBoard(Boardpos);
+                    Console.WriteLine("Enter X and Y coordinate");
+
+                    int Xcoord = Convert.ToInt32(Console.ReadLine());
+                    int Ycoord = Convert.ToInt32(Console.ReadLine());
+
+                    if (Boardpos[Xcoord - 1, Ycoord - 1] == ' ')
+                    {
+                        Boardpos[Xcoord - 1, Ycoord - 1] = Turn;
+                        TurnComplete = true;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Space Taken\n ");
+                        Console.ReadKey();
+                        Console.Clear();
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Please enter an integer between 1 and 3 inclusive");
+                    Console.ReadKey();
+                    Console.Clear();
+                }
+            }
+            
+        }
+        static char NextTurn(char Turn)
+        {
+            return (Turn == 'X') ? 'O' : 'X';
+        }
+        static bool GameEndCheck(char Turn, char[,] Boardpos, bool GameComplete)
+        {
+            int SpacesTaken = 0;
+            char Winner = ' ';
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    if (Boardpos[i, j] != ' ')
+                    {
+                        SpacesTaken++;
+                    }
+                }
+            }
+
+            for (int i = 0; i < 3; i++)
+            {
+                if (Boardpos[i, 0] == Turn && Boardpos[i, 1] == Turn && Boardpos[i, 2] == Turn)
+                {
+                    Winner = Turn;
+                    GameComplete = true;
+                }
+            }
+
+            for (int j = 0; j < 3; j++)
+            {
+                if (Boardpos[0, j] == Turn && Boardpos[1, j] == Turn && Boardpos[2, j] == Turn)
+                {
+                    Winner = Turn;
+                    GameComplete = true;
+                }
+            }
+
+            if (Boardpos[0, 0] == Turn && Boardpos[1, 1] == Turn && Boardpos[2, 2] == Turn)
+            {
+                Winner = Turn;
+                GameComplete = true;
+            }
+
+            if (Boardpos[2, 0] == Turn && Boardpos[1, 1] == Turn && Boardpos[0, 2] == Turn)
+            {
+                Winner = Turn;
+                GameComplete = true;
+            }
+
+            
+            if (SpacesTaken == 9 && Winner == ' ')
+            {
+                GameComplete = true;
+                SpawnBoard(Boardpos);
+                Console.WriteLine("Draw!");
+                Console.ReadKey();
+            }
+            else if(GameComplete == true)
+            {
+                SpawnBoard(Boardpos);
+                Console.WriteLine($"The winner is {Turn}!");
+                Console.ReadKey();
+            }
+
+            return GameComplete;
+        }
+    }   
+}
